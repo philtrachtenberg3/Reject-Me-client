81e2bcc8fd8aa947cc2ddd4b1a7fbea045b17c19
Philip Trachtenberg <philtrachtenberg@gmail.com>
2022-09-01 14:51:26 +0100

    removed more tags for deploy

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -49,10 +49,10 @@ function Navbars() {
   {loggedIn && (
     <>
         
-            <Link to="/plan/create-my-plan">
-                <Button onClick={handleSubmit} className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-lg px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            <Link to="/plan/create-my-plan" onClick={handleSubmit} className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-lg px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                
                     + Create My Plan
-                </Button>
+              
             </Link>
         
         <Navbar.Toggle />
