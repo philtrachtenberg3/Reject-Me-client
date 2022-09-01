1d24c9d5e4a0781b5af6f3639f83c7fbf4009a9c
Philip Trachtenberg <philtrachtenberg@gmail.com>
2022-09-01 12:17:39 +0100

    formatted My Plans page

--- src/pages/MyPlans/MyPlans.jsx
+++ src/pages/MyPlans/MyPlans.jsx
@@ -45,20 +45,18 @@ function MyPlans() {
 
 
   return (
-    <div className="flex justify-center align-center">
+    <div className="flex flex-col justify-center align-center items-center">
         {planList.map((singlePlanList) => {
             return (
-                <div className="w-4/12">
-                        <Card href={`/plan/my-plans/${singlePlanList._id}`} className="flex flex-col justify-center align-center">
-                            <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                                <Link to={`/plan/my-plans/${singlePlanList._id}`}>
-                                    Your plan created on: {singlePlanList.createdAt.slice(0,10)}
-                                </Link>
-                            </h5>
-                            <button type="button" onClick={() => handleDelete(singlePlanList._id)} class="w-3/12 text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
+                <div className="w-4/12 m-3">
+                    <div class="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+                        <a href={`/plan/my-plans/${singlePlanList._id}`}>
+                            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Your plan created on: {singlePlanList.createdAt.slice(0,10)}</h5>
+                        </a>
+                        <button type="button" onClick={() => handleDelete(singlePlanList._id)} class="w-3/12 text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
                                 Delete Plan
                             </button>
-                        </Card>
+                    </div>
 
                 </div>
 
